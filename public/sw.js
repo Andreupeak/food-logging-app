fe134e691df915f674462eb7acdab84266da4060
Andreupeak <ndrupk@gmail.com>
2025-11-22 17:19:31 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,13 @@
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("foodlog-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json"
-      ]);
+    caches.open("foodlog-cache-v1").then((cache) => {
+      return cache.addAll(["/", "/index.html", "/manifest.json"]);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((r) => r || fetch(event.request))
   );
 });
